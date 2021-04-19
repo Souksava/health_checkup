a1c04094daefe3036ee22b30123f34e23024c296
Souksavath PHONGPHAYOSITH <souksavath.52221881@gmail.com>
2021-04-19 14:35:28 +0700

    delete bio

--- User/Checkup/bio.php
+++ User/Checkup/bio.php
@@ -122,50 +122,30 @@
     </div>
 
 </form>
-<div class="modal fade" id="exampleModalDelete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">ຢືນຢັນການລົບຂໍ້ມູນ</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body" align="center">
-                <input type="hidden" name="id" id="id">
-                ທ່ານຕ້ອງການລົບຂໍ້ມູນ ຫຼື ບໍ່ ?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">ຍົກເລີກ</button>
-                <button type="submit" name="btnDelete" id="btnDelete" class="btn btn-outline-danger ">
-                    ລົບ
-                    <span class="" id="load_delete"></span>
-                </button>
-            </div>
-        </div>
-    </div>
-</div>
-</form>
+
 
 <?php 
  if(isset($_POST['btnDelete'])){
-    foreach($_POST['id_delete'] as $id){
-        echo"<script>";
-        echo"alert('$id');";
-        echo"</script>";
-        // $result_delete = mysqli_query($conn,"call del_biochemistry('$id2')");
+    if(isset($_POST["id_delete"])){
+        foreach($_POST['id_delete'] as $id){
+            $result_delete = mysqli_query($conn,"call del_biochemistry('$id')");
+        }
+        if(!$result_delete){
+            echo"<script>";
+            echo"window.location.href='Biochemistry?delete2=fail';";
+            echo"</script>";
+        }
+        else{
+            echo"<script>";
+            echo"window.location.href='Biochemistry?delete=success';";
+            echo"</script>";
+        }
     }
-    // if(!$result_delete){
-    //     echo"<script>";
-    //     echo"window.location.href='Biochemistry?delete2=fail';";
-    //     echo"</script>";
-    // }
-    // else{
-    //     echo"<script>";
-    //     echo"window.location.href='Biochemistry?delete=success';";
-    //     echo"</script>";
-    // }
+    else{
+        echo"<script>";
+        echo"window.location.href='Biochemistry?del=null';";
+        echo"</script>";
+    }
  }
      include ("../../header-footer/footer.php");
      if(isset($_POST["file_upload"])){
@@ -187,6 +167,11 @@
           swal("", "ການລົບຂໍ້ມູນຜິດພາດ !", "error");
           </script>';
         } 
+        if(isset($_GET["del"])=="null"){
+            echo'<script type="text/javascript">
+            swal("", "ກະລຸນາເລືອກຂໍ້ມູນທີ່ຈະລົບກ່ອນ !", "info");
+            </script>';
+          } 
 ?>
 <script>
 $("#datepicker").datepicker({
